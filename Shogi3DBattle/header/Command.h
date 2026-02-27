27447a1792f83e9bfc6ff3aa3e429b0636f63ea1
nakayama <kotaro3nya@gmail.com>
2026-02-27 16:49:37 +0900

    Commandヘッダー　概要作成

---
+++ Shogi3DBattle/header/Command.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include<d3d12.h>
+#include<wrl.h>
+
+class Command
+{
+    template<typename T>
+    using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+private:
+    ComPtr<ID3D12CommandAllocator>    _commandAllocator;
+    ComPtr<ID3D12GraphicsCommandList> _commandList;
+    ComPtr<ID3D12CommandQueue>        _commandQueue;
+
+    HRESULT CreateCommandAllocator();
+    HRESULT CreateCommandList();
+    HRESULT CreateCommandQueue();
+
+
+public:
+    HRESULT CreateCommandObject();
+};
